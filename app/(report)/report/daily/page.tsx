2e6e08e4ff1524b19d3ff922515f74ff2dd8c6b1
kyran <komnb14@gmail.com>
2026-09-07 09:17:26 +0900

    fix: keyword 수정

--- app/(report)/report/daily/page.tsx
+++ app/(report)/report/daily/page.tsx
@@ -2,7 +2,7 @@ import {Metadata} from "next";
 import ActPageContainer from "@/components/act/act-page-container";
 
 export const metadata: Metadata = {
-    title: "일일 행동 | WAVE REPORTER",
+    title: "일일 행동",
     description: "오늘 완수해야 하는 일일 행동 목록과 스펙트럼 달성 상태를 실시간으로 확인하세요.",
     openGraph: {
         title: "일일 행동 | WAVE REPORTER",
--- app/(report)/report/weekly/page.tsx
+++ app/(report)/report/weekly/page.tsx
@@ -3,7 +3,7 @@ import ActPageContainer from "@/components/act/act-page-container";
 
 
 export const metadata: Metadata = {
-    title: "주간 행동 | WAVE REPORTER",
+    title: "주간 행동",
     description: "이번 주 행동 달성 현황을 추적.",
     openGraph: {
         title: "주간 행동 | WAVE REPORTER",
